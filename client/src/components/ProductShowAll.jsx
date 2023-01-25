a13879ec4b7656758c0746c6f3532d930cd65565
Robert Lutrick <r.lutrick@icloud.com>
2023-01-25 14:45:35 -0600

    Updated to use props, handleClick, and show title

--- client/src/components/ProductShowAll.jsx
+++ client/src/components/ProductShowAll.jsx
@@ -1,42 +1,37 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-
-const ProductView = () => {
+const ProductView = (props) => {
     const [products, setProducts] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios.get('http://localhost:8001/api/product/get')
             .then(res => setProducts(res.data))
             .catch(err => console.log(`Getting data error: ${err}`))
-    }, [])
+    }, [props])
+
+    const handleClick = (e, id) => {
+        e.preventDefault()
+        navigate(`/product/${id}`)
+    }
 
     return (
         <div className='w-50 m-auto'>
             <h1>All Products:</h1>
-            <table className='table table-bordered'>
-                <thead className='table-dark'>
-                    <tr>
-                        <th>Title</th>
-                        <th>Price</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                    products.map((prod, i) => {
-                        return (
-                            <tr key={i}>
-                                <td>{prod.title}</td>
-                                <td>${prod.price}</td>
-                                <td>{prod.description}</td>
-                            </tr>
-                        )
-                    })
-                    }
-                </tbody>
-            </table>
+            {
+                products.map((prod, i) => {
+                    return (
+                        <div key={i}>
+                            <p className='btn btn-outline-dark w-50 text-capitalize' style={{cursor: "pointer"}} onClick={(e) => handleClick(e, prod._id)}>
+                                {prod.title}
+                            </p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
