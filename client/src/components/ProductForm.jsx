68793c09d14ddf0bcc69569b22ddc98d1906f56e
Robert Lutrick <r.lutrick@icloud.com>
2023-01-24 20:24:43 -0600

    Added form functionality

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 
@@ -13,39 +13,60 @@ const ProductForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         axios.post("http://localhost:8001/api/product/create", {
-            title: title, 
-            price: price, 
+            title: title,
+            price: price,
             description: description
         })
-        .then(res => navigate("/"))
-        .catch(err => console.log(`Create Page error: ${err}`))
+            .then(res => navigate("/"))
+            .catch(err => console.log(`Create Page error: ${err}`))
+
+        setTitle('');
+        setPrice('');
+        detDescription('');
     }
 
     return (
         <div>
             <h1>Product Manager</h1>
             <div className='w-50 m-auto'>
-                <form onClick={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3 row bg-dark bg-opacity-10 p-2 rounded">
                         <label className="col-sm-4 col-form-label">Title</label>
                         <div className='col-sm-8'>
-                            <input className='form-control' type="text" onChange={(e) => {setTitle(e.target.value)}} />
+                            <input
+                                className='form-control'
+                                type="text"
+                                onChange={(e) => { setTitle(e.target.value) }}
+                                value={title} />
                         </div>
                     </div>
                     <div className="mb-3 row bg-dark bg-opacity-10 p-2 rounded">
                         <label className="col-sm-4 col-form-label">Price</label>
                         <div className='col-sm-8'>
-                            <input className='form-control' type="text" onChange={(e) => {setPrice(e.target.value)}}/>
+                            <input 
+                                className='form-control' 
+                                type="text" 
+                                onChange={(e) => { setPrice(e.target.value) }} 
+                                value={price} />
                         </div>
                     </div>
                     <div className="mb-3 row bg-dark bg-opacity-10 p-2 rounded">
                         <label className="col-sm-4 col-form-label">Description</label>
                         <div className='col-sm-8'>
-                            <input className='form-control' type="text" onChange={(e) => {detDescription(e.target.value)}}/>
+                            <input 
+                                className='form-control' 
+                                type="text" 
+                                onChange={(e) => { detDescription(e.target.value) }} 
+                                value={description} />
                         </div>
                     </div>
                     <button className='btn btn-outline-dark w-50' type='submit'>Create</button>
                 </form>
+            </div>
+            <hr />
+            <div>
+                <h1>All Products:</h1>
+                <Link to={'/api/product/details'} />
             </div>
         </div>
     )
